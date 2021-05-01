d0e97024273b458169dee746520d2eacca78d776
Davi Leal <lealm.davi@gmail.com>
2021-05-01 15:50:15 -0300

    feat: add signIn to FirebaseAuth

--- lib/api/FirebaseAuth.dart
+++ lib/api/FirebaseAuth.dart
@@ -4,12 +4,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class FirebaseAuth with ChangeNotifier {
-  static const _url =
-      "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBPPA7F1zvxnrYQQU9BuERbLNZ55T2OHaE";
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyBPPA7F1zvxnrYQQU9BuERbLNZ55T2OHaE";
 
-  Future<void> signUp(String email, String password) async {
     final response = await http.post(
-      Uri.parse(_url),
+      Uri.parse(url),
       body: json.encode({
         "email": email,
         "password": password,
@@ -20,5 +21,13 @@ class FirebaseAuth with ChangeNotifier {
     print(json.decode(response.body));
 
     return Future.value();
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, "signUp");
+  }
+
+  Future<void> signIn(String email, String password) async {
+    return _authenticate(email, password, "signInWithPassword");
   }
 }
