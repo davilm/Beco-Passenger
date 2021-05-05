53c91d80981fa9615d26ca91cdc2087cfec068dd
Davi Leal <lealm.davi@gmail.com>
2021-05-05 16:18:28 -0300

    feat: update HomeScreen

--- lib/views/HomeScreen/HomeScreen.dart
+++ lib/views/HomeScreen/HomeScreen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:tela_de_login_beco/main.dart';
-import 'package:tela_de_login_beco/views/ChooseSign/ChooseSign.dart';
+import 'package:tela_de_login_beco/views/HomeScreen/widgets/CreateRouteWidget.dart';
 import 'package:tela_de_login_beco/views/HomeScreen/widgets/DrawerWidget.dart';
+import 'package:tela_de_login_beco/views/HomeScreen/widgets/WorkWidget.dart';
 
 class HomeScreen extends StatefulWidget {
+  HomeScreen({Key key}) : super(key: key);
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
@@ -13,7 +15,6 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      backgroundColor: Colors.red,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
@@ -21,8 +22,44 @@ class _HomeScreenState extends State<HomeScreen> {
           color: Colors.black,
         ),
       ),
-      body: Center(child: Text("Content")),
       drawer: DrawerWidget(),
+      body: Center(
+        child: Container(
+          height: 700,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                  "https://media.istockphoto.com/vectors/vector-city-map-vector-id826280312?k=6&m=826280312&s=170667a&w=0&h=Sa_vE8LCTr9ze5cLsmotHB2vrHHf6J0N6W4ADWDCq7c="),
+              fit: BoxFit.fitHeight,
+            ),
+          ),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Positioned(
+                bottom: MediaQuery.of(context).size.height / 5.2,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Color(0xff36B194),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(18),
+                    ),
+                  ),
+                  height: 200,
+                  child: WorkWidget(),
+                ),
+              ),
+              Positioned(
+                child: Container(
+                  height: MediaQuery.of(context).size.height / 2.8,
+                  color: Colors.blue,
+                  child: CreateRouteWidget(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
