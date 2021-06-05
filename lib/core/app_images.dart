4cf7dc7ddabb1af0f0965cab609df2c65a3a8840
Davi Leal <lealm.davi@gmail.com>
2021-06-05 15:14:01 -0300

    feat: add image shortcuts

---
+++ lib/core/app_images.dart
@@ -0,0 +1,21 @@
+class AppImages {
+  static String get faceLight => 'assets/images/face-light.png';
+  static String get faceDark => 'assets/images/face-dark.png';
+  static String get configLight => 'assets/images/config-light.png';
+  static String get configDark => 'assets/images/config-dark.png';
+  static String get notificationLight => 'assets/images/bell-light.png';
+  static String get notificationDark => 'assets/images/bell-dark.png';
+  static String get moonLight => 'assets/images/moon-light.png';
+  static String get moonDark => 'assets/images/moon-dark.png';
+  static String get travelLight => 'assets/images/travel-light.png';
+  static String get travelDark => 'assets/images/travel-dark.png';
+  static String get signoutLight => 'assets/images/signout-light.png';
+  static String get signoutDark => 'assets/images/signout-dark.png';
+  static String get languageLight => 'assets/images/language-light.png';
+  static String get languageDark => 'assets/images/language-dark.png';
+  static String get helpLight => 'assets/images/help-light.png';
+  static String get helpDark => 'assets/images/help-dark.png';
+  static String get gardenCity => 'assets/images/garden-city.png';
+  static String get confirm => 'assets/images/confirm.png';
+  static String get chat => 'assets/images/chat.png';
+}
