cf1e53c0adce76fca2e68577859e82be660bfab3
Davi Leal <lealm.davi@gmail.com>
2021-06-05 15:18:16 -0300

    feat: add ConfigurationScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tela_de_login_beco/views/ConfigurationScreen/ConfigurationScreen.dart';
 import 'package:tela_de_login_beco/views/HomeScreen/HomeScreen.dart';
 import 'package:tela_de_login_beco/views/Splashscreen/Splashscreen.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: HomeScreen(),
+    // home: HomeScreen(),
+    home: ConfigurationScreen(),
     debugShowCheckedModeBanner: false,
     theme: ThemeData(
         primaryColor: Colors.black,
