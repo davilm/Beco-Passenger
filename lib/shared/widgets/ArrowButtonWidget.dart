bc2b63e4f18636573c4d7d6b7315d57b3cfc4b8a
Davi Leal <lealm.davi@gmail.com>
2021-06-09 10:47:12 -0300

    feat: add ArrowButtonWidget

---
+++ lib/shared/widgets/ArrowButtonWidget.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ArrowButtonWidget extends StatefulWidget {
+  final Function func;
+
+  ArrowButtonWidget(this.func, {Key key}) : super(key: key);
+
+  @override
+  _ArrowButtonWidgetState createState() => _ArrowButtonWidgetState();
+}
+
+class _ArrowButtonWidgetState extends State<ArrowButtonWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 30),
+      child: Container(
+        width: 42,
+        height: 42,
+        decoration: BoxDecoration(
+          color: Color(0xffF5F5F7),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: IconButton(
+          icon: Icon(
+            Icons.arrow_forward_ios,
+            size: 12,
+          ),
+          onPressed: widget.func,
+        ),
+      ),
+    );
+  }
+}
