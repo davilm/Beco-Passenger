34cbe279a4e4214b5f9445b3ccf522163dd74e8c
Davi Leal <lealm.davi@gmail.com>
2021-06-27 00:20:21 -0300

    fix: Authentication

--- lib/api/Authentication.dart
+++ lib/api/Authentication.dart
@@ -6,6 +6,11 @@ class Authentication {
 
   late UserCredential userCredential;
 
+  Future<String> signOut() async {
+    await _auth.signOut();
+    return "Sign Out";
+  }
+
   Future<void> signUp(
     String email,
     String name,
@@ -19,16 +24,23 @@ class Authentication {
       'name': name,
       'cpf': cpf,
       'email': email,
+      'imageUrl':
+          "https://firebasestorage.googleapis.com/v0/b/beco-9fab4.appspot.com/o/face-light.png?alt=media&token=ceaba8cb-80ec-4f20-a56f-976f16d22216",
     };
 
     await FirebaseFirestore.instance
-        .collection('users')
+        .collection('passengers')
         .doc(userCredential.user!.uid)
         .set(userData);
   }
 
-  Future<void> signIn(String email, String password) async {
-    userCredential = await _auth.signInWithEmailAndPassword(
-        email: email, password: password);
+  Future<String> signIn(String email, String password) async {
+    try {
+      userCredential = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return "Signed in";
+    } on FirebaseAuthException catch (e) {
+      return e.message.toString();
+    }
   }
 }
