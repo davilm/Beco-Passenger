77db00e0d755d2cefd9956e7c9cbacb608759b7c
Davi Leal <lealm.davi@gmail.com>
2021-07-05 13:58:09 -0300

    feat: add geo location

---
+++ lib/controllers/geo_location_controller.dart
@@ -0,0 +1,34 @@
+import 'package:geolocator/geolocator.dart';
+
+class GeoLocationController {
+  final latitude = 0.0;
+  final longitude = 0.0;
+
+  Future<Position> currentPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    // print(Geolocator.getCurrentPosition());
+
+    return await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+  }
+}
