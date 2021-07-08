a2e5ccb9ff308133ba4e86e4ac69d9133cdec63f
Davi Leal <lealm.davi@gmail.com>
2021-07-08 02:41:55 -0300

    fix: change style

--- lib/views/HomeScreen/widgets/SetDestination.dart
+++ lib/views/HomeScreen/widgets/SetDestination.dart
@@ -1,3 +1,4 @@
+import 'package:beco_passenger/core/core.dart';
 import 'package:flutter/material.dart';
 
 class SetDestination extends StatefulWidget {
@@ -24,6 +25,8 @@ class _SetDestinationState extends State<SetDestination> {
 
   @override
   Widget build(BuildContext context) {
+    final double widthMargin = MediaQuery.of(context).size.width / 40;
+
     final generalWidth = MediaQuery.of(context).size.width;
     final double fieldButtonHeight = 60;
     final String selectedRoute = "Gwo9mVet7JJMi2Je8yRw";
@@ -33,130 +36,142 @@ class _SetDestinationState extends State<SetDestination> {
     return Container(
       width: MediaQuery.of(context).size.width,
       color: Colors.white,
-      child: Column(
+      child: Stack(
         children: [
-          Padding(
-            padding: EdgeInsets.only(
-                left: widthMarginBody, right: 30, top: 4, bottom: 16),
-            child: Container(
-              height: fieldButtonHeight,
-              width: generalWidth,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Color(0xffD0D2DA),
-                  width: 1,
+          Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(
+                  left: widthMargin * 5,
+                  right: widthMargin * 5,
+                  bottom: 10,
                 ),
-                color: Colors.white,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 15),
-                    child: Icon(
-                      Icons.gps_fixed,
-                      color: Color(0xff92959E),
-                      size: 30.0,
+                child: Container(
+                  height: fieldButtonHeight,
+                  width: generalWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Color(0xffD0D2DA),
+                      width: 1,
                     ),
+                    color: Colors.white,
                   ),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Fortaleza",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: Color(0xff92959E),
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-                left: widthMarginBody, right: 30, top: 4, bottom: 16),
-            child: Container(
-              height: fieldButtonHeight,
-              width: generalWidth,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                border: Border.all(
-                  color: Color(0xffD0D2DA),
-                  width: 1,
-                ),
-                color: Colors.white,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20, right: 15),
-                    child: Icon(
-                      Icons.place,
-                      color: Color(0xff92959E),
-                      size: 30.0,
-                    ),
-                  ),
-                  Container(
-                    width: MediaQuery.of(context).size.width / 2,
-                    child: DropdownButton(
-                      underline: Container(color: Colors.transparent),
-                      icon: Icon(
-                        Icons.arrow_drop_down,
-                        color: Colors.transparent,
-                      ),
-                      items: cityList.map((city) {
-                        return DropdownMenuItem(
-                          value: city,
-                          child: Text(city),
-                        );
-                      }).toList(),
-                      hint: Text(
-                        cityName,
-                        style: TextStyle(
-                          fontSize: 16,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 15),
+                        child: Icon(
+                          Icons.gps_fixed,
                           color: Color(0xff92959E),
-                          fontWeight: FontWeight.w600,
+                          size: 30.0,
                         ),
                       ),
-                      onChanged: (city) {
-                        setState(() {
-                          cityName = city.toString();
-                        });
-                      },
-                    ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Fortaleza",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color(0xff92959E),
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
-            ),
+              Padding(
+                padding: EdgeInsets.only(
+                  left: widthMargin * 5,
+                  right: widthMargin * 5,
+                  bottom: 20,
+                ),
+                child: Container(
+                  height: fieldButtonHeight,
+                  width: generalWidth,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Color(0xffD0D2DA),
+                      width: 1,
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 20, right: 15),
+                        child: Icon(
+                          Icons.place,
+                          color: Color(0xff92959E),
+                          size: 30.0,
+                        ),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        child: DropdownButton(
+                          underline: Container(color: Colors.transparent),
+                          icon: Icon(
+                            Icons.arrow_drop_down,
+                            color: Colors.transparent,
+                          ),
+                          items: cityList.map((city) {
+                            return DropdownMenuItem(
+                              value: city,
+                              child: Text(city),
+                            );
+                          }).toList(),
+                          hint: Text(
+                            cityName,
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Color(0xff92959E),
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          onChanged: (city) {
+                            setState(() {
+                              cityName = city.toString();
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
           Padding(
             padding: EdgeInsets.only(
-                left: widthMarginBody, right: 30, top: 4, bottom: 16),
-            child: Container(
-              height: fieldButtonHeight,
-              width: generalWidth,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+              left: widthMargin * 5,
+              right: widthMargin * 5,
+              bottom: 20,
+            ),
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Color(0xff15192C),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
-                child: Text(
-                  "Definir Destino",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
+                  child: Text(
+                    "Definir Destino",
+                    style: AppTextStyles.montserrat14SemiboldWhite,
                   ),
+                  onPressed: () {
+                    widget.onChoosedRoute();
+                  },
                 ),
-                onPressed: () {
-                  widget.onChoosedRoute();
-                },
               ),
             ),
           ),
