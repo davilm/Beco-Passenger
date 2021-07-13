6a72edbb36117e2746c12a6ff645e9fea6dafe3e
Davi Leal <lealm.davi@gmail.com>
2021-07-13 02:57:20 -0300

    feat: add getLatLng

--- lib/controllers/geocoding_repository.dart
+++ lib/controllers/geocoding_repository.dart
@@ -1,6 +1,6 @@
 import 'package:beco_passenger/models/geocoding_model.dart';
+import 'package:beco_passenger/models/reverse_geocoding_model.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:beco_passenger/.env.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
@@ -13,12 +13,12 @@ class GeocodingRepository {
   GeocodingRepository({Dio? dio}) : _dio = dio ?? Dio();
 
   Future<Geocoding> getAddress({
-    @required LatLng? latlng,
+    required LatLng latlng,
   }) async {
     final response = await _dio.get(
       _baseUrl,
       queryParameters: {
-        'latlng': '${latlng!.latitude},${latlng.longitude}',
+        'latlng': '${latlng.latitude},${latlng.longitude}',
         'key': googleAPIKey,
       },
     );
@@ -26,6 +26,24 @@ class GeocodingRepository {
       print('${latlng.latitude},${latlng.longitude}');
       print(response.statusCode);
       return Geocoding.fromMap(response.data);
+    }
+    return Future.error('Ops, problem with google api');
+  }
+
+  Future<ReverseGeocoding> getLatLng({
+    required String address,
+  }) async {
+    final response = await _dio.get(
+      _baseUrl,
+      queryParameters: {
+        'address': '$address',
+        'key': googleAPIKey,
+      },
+    );
+    if (response.statusCode == 200) {
+      print('$address');
+      print(response.statusCode);
+      return ReverseGeocoding.fromMap(response.data);
     }
     return Future.error('Ops, problem with google api');
   }
